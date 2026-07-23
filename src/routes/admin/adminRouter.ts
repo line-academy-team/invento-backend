c3ab7a7171eb9e5d3d33ce213158febb8c384fa7
hyw0206 <hyw0206s@naver.com>
2026-07-23 09:45:20 +0900

    어드민 라우터 수정사항

--- src/routes/admin/adminRouter.ts
+++ src/routes/admin/adminRouter.ts
@@ -6,4 +6,16 @@ const router = Router();
 router.use(authenticate);
 router.use(requiredAdmin);
 
+// 어드민 생성 -> 유저로 가입 -> DataGrip에서 User.role 수정
+
+// 어드민 로그인
+
+// 사용자 목록 조회
+
+// 사용자 상세 조회
+
+// 계정 삭제 + 복구 -> updateUser
+
+// 단체 정지 + 복구 -> updateOrganization
+
 export default router;
